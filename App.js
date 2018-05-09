5d70753b7ccb6c610f32b10c1801c9f0fd6ba493
jhonvillamil88 <jvillamil@geinsys.com>
2018-05-08 19:34:17 -0500

    Mapa funcionando en entorno de la oficina

--- App.js
+++ App.js
@@ -10,21 +10,28 @@ import MapView from 'react-native-maps';
 
 
 export default class App extends Component {
-
+//, Longitud:
   state = {
     region: {
-      latitude: 37.78825,
-      longitude: -122.4324,
+      latitude: 3.42158,
+      longitude: -76.5205,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     },
   };
+
   render() {
+    location = {
+      longitude: -76.509620,
+      latitude: 3.395037
+    }
     return (
       <MapView
         style={styles.map}
         region={this.state.region}
-      />
+      >
+        <MapView.Marker  coordinate={location} />
+      </MapView>
     );
   }
 }
